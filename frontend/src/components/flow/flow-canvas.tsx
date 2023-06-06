1ab638e888f988350f1d24c55d9577ade97f13fa
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-06-06 21:04:16 +0200

    chore: refactored page structure

--- frontend/src/components/flow/flow-canvas.tsx
+++ frontend/src/components/flow/flow-canvas.tsx
@@ -1,11 +1,6 @@
 import { Background, BackgroundVariant } from '@reactflow/background';
 import { Controls } from '@reactflow/controls';
-import {
-	ConnectionMode,
-	ReactFlow,
-	ReactFlowInstance,
-	ReactFlowProvider,
-} from '@reactflow/core';
+import { ConnectionMode, ReactFlow, ReactFlowInstance } from '@reactflow/core';
 import {
 	memo,
 	MemoExoticComponent,
@@ -21,14 +16,21 @@ import {
 	InternalNode,
 	ServiceNode,
 } from '@/components/flow/nodes';
+import {
+	DEFAULT_FLOW_HEIGHT,
+	NAV_BAR_HEIGHT_PIXELS,
+	RAIL_WIDTH_PIXELS,
+	REM,
+} from '@/constants';
 import { ThemeContext } from '@/context';
 import { FlowStoreState, useStore } from '@/hooks/use-store';
+import { useWindowsDimensions } from '@/hooks/use-window-dimensions';
 
 export interface FlowProps {
 	connectionMode: ConnectionMode;
-	containerHeight: number;
-	containerWidth: number;
 	dndRef: (element: HTMLDivElement) => void;
+	isExpandedNode: boolean;
+	isFullWidth: boolean;
 	setReactFlowInstance: (reactFlowInstance: ReactFlowInstance) => void;
 	showBackground: boolean;
 }
@@ -52,10 +54,33 @@ const edgeTypes: Record<string, MemoExoticComponent<any>> = {
 	HttpRestEdge: memo(HttpRestEdge),
 };
 
+const calculateFlowHeight = (
+	windowHeight: number,
+	isExpandedNode: boolean,
+): number => {
+	const flowHeight =
+		windowHeight -
+		(isExpandedNode
+			? NAV_BAR_HEIGHT_PIXELS * 2 + REM
+			: NAV_BAR_HEIGHT_PIXELS);
+
+	return flowHeight > 0 ? flowHeight : DEFAULT_FLOW_HEIGHT;
+};
+
+const calculateFlowWidth = (
+	windowWidth: number,
+	isFullWith: boolean,
+): number => {
+	const flowWidth =
+		windowWidth -
+		(isFullWith ? RAIL_WIDTH_PIXELS / 2 : RAIL_WIDTH_PIXELS * 2);
+	return flowWidth > 0 ? flowWidth : DEFAULT_FLOW_HEIGHT;
+};
+
 export function Flow({
 	connectionMode,
-	containerHeight,
-	containerWidth,
+	isFullWidth,
+	isExpandedNode,
 	dndRef,
 	setReactFlowInstance,
 	showBackground,
@@ -71,55 +96,72 @@ export function Flow({
 		onConnect,
 	} = useStore(flowStateSelector, shallow);
 
+	// theming
+
 	const [backgroundColor, setBackgroundColor] = useState('yellow');
 
 	useEffect(() => {
 		/*
     We have to access the document to get the active theme css values.
     We access the 'secondary' color value using --s.
-    * */
+    */
 		const root = document.querySelector(':root');
 		const color = root && getComputedStyle(root).getPropertyValue('--s');
 		setBackgroundColor(color ? `hsl(${color})` : 'yellow');
 	}, [theme.currentTheme]);
 
+	// flow container sizing
+	const [windowHeight, windowWidth] = useWindowsDimensions();
+	const [flowHeight, setFlowHeight] = useState(
+		calculateFlowHeight(windowHeight, false),
+	);
+	const [flowWidth, setFlowWidth] = useState(
+		calculateFlowWidth(windowWidth, false),
+	);
+
+	useEffect(() => {
+		setFlowHeight(calculateFlowHeight(windowHeight, isExpandedNode));
+	}, [windowHeight, isExpandedNode]);
+
+	useEffect(() => {
+		setFlowWidth(calculateFlowWidth(windowWidth, isFullWidth));
+	}, [windowWidth, isFullWidth]);
+
 	return (
-		<ReactFlowProvider>
-			<div
-				ref={dndRef}
-				style={{
-					// NOTE: we are forced to pass style here because reactflow has issues with setting the container heights
-					// using classes. These values are calculated in the parent to be responsive to window size changes.
-					height: containerHeight,
-					width: containerWidth,
-				}}
+		<div
+			ref={dndRef}
+			style={{
+				// NOTE: we are forced to pass style here because reactflow has issues with setting the container heights
+				// using classes. These values are calculated in the parent to be responsive to window size changes.
+				height: flowHeight,
+				width: flowWidth,
+			}}
+		>
+			<ReactFlow
+				connectionLineComponent={ConnectionLine}
+				connectionMode={connectionMode}
+				edgeTypes={edgeTypes}
+				edges={edges}
+				fitView={true}
+				nodeTypes={nodeTypes}
+				nodes={nodes}
+				onConnect={onConnect}
+				onEdgeUpdate={onEdgeUpdate}
+				onEdgesChange={onEdgesChange}
+				onInit={setReactFlowInstance}
+				onNodesChange={onNodesChange}
+				proOptions={{ hideAttribution: true }}
+				snapToGrid={true}
 			>
-				<ReactFlow
-					connectionLineComponent={ConnectionLine}
-					connectionMode={connectionMode}
-					edgeTypes={edgeTypes}
-					edges={edges}
-					fitView={true}
-					nodeTypes={nodeTypes}
-					nodes={nodes}
-					onConnect={onConnect}
-					onEdgeUpdate={onEdgeUpdate}
-					onEdgesChange={onEdgesChange}
-					onInit={setReactFlowInstance}
-					onNodesChange={onNodesChange}
-					proOptions={{ hideAttribution: true }}
-					snapToGrid={true}
-				>
-					<Controls className="bg-accent focus:bg-accent-content border-black" />
-					{showBackground && (
-						<Background
-							variant={BackgroundVariant.Dots}
-							color={backgroundColor}
-							size={1.5}
-						/>
-					)}
-				</ReactFlow>
-			</div>
-		</ReactFlowProvider>
+				<Controls className="bg-accent focus:bg-accent-content border-black" />
+				{showBackground && (
+					<Background
+						variant={BackgroundVariant.Dots}
+						color={backgroundColor}
+						size={1.5}
+					/>
+				)}
+			</ReactFlow>
+		</div>
 	);
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { ReactFlowInstance, ReactFlowProvider } from '@reactflow/core';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useEffect, useState } from 'react';
+import { ConnectionMode } from 'reactflow';
 
-import { FlowContainer } from '@/components/flow/flow-container';
+import { Flow } from '@/components/flow/flow-canvas';
+import { InternalFlowHeader } from '@/components/flow/internal-flow-header';
+import { NodeForm } from '@/components/forms/node-form';
 import { Navbar } from '@/components/navbar';
+import { PromptContainer } from '@/components/prompt/prompt-container';
+import { SideRail } from '@/components/side-menu/side-rail';
+import { useBoundedDrop } from '@/hooks/use-bounded-drop';
+import {
+	useConfiguredNode,
+	useExpandedNode,
+	useInsertNode,
+	useUnsetConfiguredNode,
+} from '@/hooks/use-store';
+import { createNode } from '@/utils/node';
 
 export async function getStaticProps({ locale }: { locale: string }) {
 	return {
@@ -32,8 +47,45 @@ export default function Index() {
 		return;
 	};
 
+	const insertNode = useInsertNode();
+	const configuredNode = useConfiguredNode();
+	const expandedNode = useExpandedNode();
+	const unsetConfiguredNode = useUnsetConfiguredNode();
+
+	const [isClientSide, setIsClientSide] = useState(false);
+	const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+	const [isPromptModalOpen, setIsPromptModalOpen] = useState(false);
+
+	const [reactFlowInstance, setReactFlowInstance] =
+		useState<ReactFlowInstance | null>(null);
+
+	const [dndDropData, dndRef] = useBoundedDrop();
+
+	useEffect(() => {
+		// useEffect is executed only on the client side.
+		// we need to render the react-flow components only in SPA mode.
+		setIsClientSide(true);
+	}, []);
+
+	// drag and drop
+	useEffect(() => {
+		if (dndDropData && reactFlowInstance) {
+			const { nodeType, x, y } = dndDropData;
+
+			insertNode(
+				createNode({
+					position: reactFlowInstance.project({ x, y }),
+					data: {
+						nodeType,
+						formData: { nodeName: 'Unnamed' },
+					},
+				}),
+			);
+		}
+	}, [dndDropData, reactFlowInstance]);
+
 	return (
-		<div>
+		<ReactFlowProvider>
 			<Navbar
 				onBurgerIconClick={handleBurgerIconClick}
 				onDownloadIconClick={handleDownloadIconClick}
@@ -42,7 +94,51 @@ export default function Index() {
 				onUserIconClick={handleUserIconClick}
 				projectName="Backend Example"
 			/>
-			<FlowContainer />
-		</div>
+			<main className="h-full w-full flex justify-between">
+				<PromptContainer
+					closePromptModal={() => {
+						setIsPromptModalOpen(false);
+					}}
+					isPromptModalOpen={isPromptModalOpen}
+				/>
+				<SideRail
+					isMenuOpen={isSideMenuOpen}
+					setIsMenuOpen={setIsSideMenuOpen}
+					togglePromptModal={() => {
+						setIsPromptModalOpen(!isPromptModalOpen);
+					}}
+				/>
+				<div
+					className={`h-full transition-all duration-300 ease-in-out ${
+						expandedNode
+							? 'bg-base-100 rounded border-2 border-neutral'
+							: 'bg-base-300'
+					} ${isSideMenuOpen ? 'shrink' : 'grow'}`}
+				>
+					{expandedNode && (
+						<InternalFlowHeader {...expandedNode.data} />
+					)}
+					{isClientSide && (
+						<Flow
+							connectionMode={ConnectionMode.Loose}
+							dndRef={dndRef}
+							isExpandedNode={!!expandedNode}
+							isFullWidth={!isSideMenuOpen}
+							setReactFlowInstance={setReactFlowInstance}
+							showBackground={!expandedNode}
+						/>
+					)}
+				</div>
+				{configuredNode && (
+					<div className="absolute inset-1 w-5/10 h-full z-10 flex justify-center">
+						<NodeForm
+							closeMenuHandler={() => {
+								unsetConfiguredNode();
+							}}
+						/>
+					</div>
+				)}
+			</main>
+		</ReactFlowProvider>
 	);
 }
