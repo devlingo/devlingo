a4818d19a2b5d34ed106430d4ef2d461eb99ab37
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-06-20 16:47:20 +0200

    chore: updated coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
 		coverage: {
 			reporter: ['text', 'json-summary', 'json'],
+			exclude: [
+				...(configDefaults.coverage.exclude ?? []),
+				'frontend/tests/**/*.*',
+				'backend/tests/**/*.*',
+			],
 		},
 	},
 });
