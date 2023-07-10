31eba1bc0c36137830390cec385d0adf7bca958c
isaac_lindenman <tomisaaccohen@gmail.com>
2023-07-10 09:51:53 +0300

    chore(DEVLINGO-13): add getServerSide props for translations at index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,16 @@
 import 'firebaseui/dist/firebaseui.css';
 
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 import { SignInScreen } from '@/components/sign-in-page/sign-in-screen';
 
+export async function getServerSideProps({ locale }: { locale: string }) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['sign-in'])),
+		},
+	};
+}
 export default function Index() {
 	return <SignInScreen />;
 }
