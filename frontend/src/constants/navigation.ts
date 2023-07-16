3ba527c3e7319413a22d082851c649e7da633308
isaac_lindenman <tomisaaccohen@gmail.com>
2023-07-16 16:49:59 +0300

    feat(DEVLINGO-21): expanded navigation enum

--- frontend/src/constants/navigation.ts
+++ frontend/src/constants/navigation.ts
@@ -1,5 +1,17 @@
 export enum Navigation {
 	Base = '/',
+	//application
 	Projects = '/projects',
 	Design = '/design',
+	SignIn = '/',
+	//website
+	About = '/about',
+	Contact = 'mailto:tom@devlingo.net',
+	Blog = '/blog',
+	TOS = '/terms-of-service',
+	PrivacyPolicy = '/privacy-policy',
+	//external
+	Discord = 'https://discord.gg/ynxGsRnfbm ',
+	Linkedin = '/soon',
+	Twitter = '/soon',
 }
